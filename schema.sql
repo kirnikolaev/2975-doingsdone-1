1f396d6094dc91af7a427d277732282792c327fe
Kirnikolaev <t79217650125@gmail.com>
2018-05-19 18:07:17 +0300

    добавил индексы уникальности

--- schema.sql
+++ schema.sql
@@ -1,19 +1,19 @@
 CREATE DATABASE doings
-	DEFAULT CHARACTER SET utf8
-	DEFAULT COLLATE utf8_general_ci;
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
 
 USE doings;
 
 CREATE TABLE `projects` (
 id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-`project_name` CHAR(64)
+`project_name` CHAR(16)
 );
 
 CREATE TABLE `tasks` (
 id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`create_time` DATETIME,
 	`execution_date` DATE,
-	`task_title` CHAR(255),
+	`task_title` CHAR(64),
 	`due_date` DATETIME,
 	`file_link` CHAR(64)
 );
@@ -21,9 +21,12 @@ id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 CREATE TABLE `users` (
 id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`registration_date` DATE,
-	`name` CHAR(62),
-	`password` CHAR(32),
-	`email` CHAR(32),
+	`name` CHAR(18),
+	`password` CHAR(18),
+	`email` CHAR(18),
 	`phone` TINYINT(13)
-
 );
+
+CREATE UNIQUE INDEX email ON users(email);
+CREATE UNIQUE INDEX phone ON users(phone);
+CREATE INDEX task_title ON tasks(task_title);
